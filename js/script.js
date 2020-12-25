745099c0b5d684768eb8622c13e31d3c76b3ceb0
Uktam <uktam.khamidov@stanum.us>
2020-12-25 16:19:37 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+$(document).ready(function () {
+  $('.stages__slider').slick({
+    prevArrow: '.stages__slider-prev',
+    nextArrow: '.stages__slider-next',
+    infinite: false,
+    dots: true,
+    appendDots: '.stages__title',
+    //fade: true
+  });
+  $('.stages__slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    const dots = document.querySelectorAll('.slick-dots li')
+    for (let i = 0; i <= dots.length - 1; i++) {
+      if (i <= nextSlide) {
+        dots[i].classList.add('active');
+      }
+      else {
+        dots[i].classList.remove('active');
+      }
+    }
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.querySelector('.header__menuBtn');
   const menu = document.querySelector('.header__menu');
