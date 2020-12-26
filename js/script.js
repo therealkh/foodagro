573acccdcd3bd36a43c6a942270d16bed75d4740
Uktam <uktam.khamidov@stanum.us>
2020-12-26 20:36:14 +0500

    mailer done

--- js/script.js
+++ js/script.js
@@ -26,20 +26,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.querySelector('.header__menuBtn');
   const menu = document.querySelector('.header__menu');
   const ppOpeners = document.querySelectorAll('.popup-open');
+  const ppClosers = document.querySelectorAll('.popup-close');
   const popups = document.querySelectorAll('.popup');
   const lockPadding = document.querySelectorAll('.lock-padding');
   const body = document.querySelector('body');
-  const form = document.querySelector('#sendForm');
   const menuLinks = document.querySelectorAll('.header__menu>ul>li>a');
   //const decorsLeft = document.querySelectorAll('.decor-left');
   const decoratedBlocks = document.querySelectorAll('.decorated');
+  const form = document.querySelector('form#sendForm');
+  const catalogForm = document.querySelector('form#sendCatalog');
+  console.dir(form);
+  form.onsubmit = (event) => {
+    send(form, event, 'mailer/sendForm.php', 'Спасибо! Ваша заявка была отправлена.')
+  }
+  catalogForm.onsubmit = (event) => {
+    send(catalogForm, event, 'mailer/sendCatalog.php', 'Спасибо за Ваш интерес. Скоро вышлем каталог!')
+  }
+
+  //form.addEventListener('sumbit', (e) => {
+  //})
   console.log(decoratedBlocks);
   document.addEventListener('mousemove', event => {
     if (event.target.closest('.decorated')) {
       block = event.target.closest('.decorated');
       const decorLeft = block.querySelector('.decor-left');
       const decorRight = block.querySelector('.decor-right');
-      console.log(`Y(${event.clientY}) X(${event.clientX})`);
+      //console.log(`Y(${event.clientY}) X(${event.clientX})`);
       const width = block.offsetWidth;
       decorLeft.style.transform = `rotateY(${(event.clientX / 50)}deg) rotateX(${(-event.clientY / 50)}deg)`;
       decorRight.style.transform = `rotateY(${-1 * ((width - event.clientX) / 50)}deg) rotateX(${(-event.clientY / 50)}deg)`;
@@ -57,10 +69,6 @@ document.addEventListener("DOMContentLoaded", () => {
         onBurgerClicked();
       }
     })
-  })
-  form.addEventListener('submit', () => {
-    console.log('121314adsasdasdas324211');
-    alert(123123);
   })
   let isMobileMenu = false;
   let unlock = true;
@@ -83,6 +91,15 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
       })
     }
+  }
+  if (ppClosers.length > 0) {
+    ppClosers.forEach((item, index, arr) => {
+      item.addEventListener('click', (event) => {
+        const activePopup = document.querySelector('.popup.opened');
+        if (activePopup) { closePopup(activePopup) }
+        event.preventDefault();
+      })
+    })
   }
   if (popups.length > 0) {
     for (let i = 0; i < popups.length; i++) {
@@ -118,6 +135,11 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
+  function openResultPopup(msg = 'Спасибо! Ваша заявка была отправлена.') {
+    const msgNode = document.querySelector('#result .result-msg');
+    msgNode.textContent = msg;
+    openPopup(document.querySelector('#result'));
+  }
   function bodyLock() {
     const lockPaddingValue = window.innerWidth - document.querySelector('.intro').offsetWidth + 'px';
     //console.log(lockPaddingValue);
@@ -151,6 +173,7 @@ document.addEventListener("DOMContentLoaded", () => {
       unlock = true;
     }, timeout);
   }
+
   document.addEventListener('keydown', e => {
     if (e.key === 'Escape') {
       const activePopup = document.querySelector('.popup.opened');
@@ -178,8 +201,45 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.style.overflow = '';
     isMobileMenu = false;
   }
+  function send(form, event, php, succesMSG) {
+    const btn = form.querySelector('#formSubmit');
 
+    const oldTextContent = btn.textContent;
+    btn.textContent = 'Отправка...';
+    btn.setAttribute('disabled', 'disabled');
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function () {
+      if (req.status >= 200 && req.status < 400) {
+        json = JSON.parse(this.response); // Ебанный internet explorer 11
+        console.log(json);
 
+        // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+        if (json.result == "success") {
+          // Если сообщение отправлено
+          btn.textContent = oldTextContent;
+          btn.removeAttribute('disabled');
+          openResultPopup(succesMSG);
+        } else {
+          // Если произошла ошибка
+          btn.textContent = oldTextContent;
+          btn.removeAttribute('disabled');
+          openResultPopup(`Ой... Ошибка. Сообщение не отправлено (${json.result})`);
+        }
+        // Если не удалось связаться с php файлом
+      } else {
+        btn.textContent = oldTextContent;
+        btn.removeAttribute('disabled');
+        openResultPopup('Ошибка сервера. Код ошибки: ' + req.status);
+        //alert("Ошибка сервера. Номер: " + req.status);
+      }
+    };
 
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function () { alert("Ошибка отправки запроса"); };
+    req.send(new FormData(event.target));
+  }
 
 })
---
+++ mailer/sendCatalog.php
@@ -0,0 +1,67 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$username = $_POST['name'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "Клиент просит выслать каталог.";
+$body = "
+<b>Имя:</b> $username<br>
+<b>Почта:</b> $email<br><br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'mailer.stanum.delta'; // Логин на почте
+    $mail->Password   = 'stanum159357'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('mailer.stanum.delta@mail.ru', 'FoodAgro Mailer'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('uktam4ik.khamidov@gmail.com');  
+    //$mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+    // Прикрипление файлов к письму
+//if (!empty($file['name'][0])) {
+//    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+//        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+//        $filename = $file['name'][$ct];
+//        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+//            $mail->addAttachment($uploadfile, $filename);
+//            $rfile[] = "Файл $filename прикреплён";
+//        } else {
+//            $rfile[] = "Не удалось прикрепить файл $filename";
+//        }
+//    }   
+//}
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
